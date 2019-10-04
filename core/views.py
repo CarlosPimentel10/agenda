4cef315fdcfccb1aeb9d1cda5fb4ad1afba5da04
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2019-10-04 13:36:20 +0100

    Incluido template para insercao de eventos da agenda

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
+
 # Create your views here.
 
 
@@ -31,6 +32,7 @@ def submit_login(request):
             messages.error(request, "Usuário ou senha inválido")
         return redirect('/')
 
+
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
@@ -38,3 +40,21 @@ def lista_eventos(request):
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+        return redirect('/')
